2a7056f1d9037761fc38f96ee41de53b747c2bd6
dsemenov <dmytro.semenov@disneystreaming.com>
2021-02-25 14:33:06 -0500

    fix tests

--- tests/src/main/scala/io/github/howardjohn/scanamo/DynamoFormatBehavior.scala
+++ tests/src/main/scala/io/github/howardjohn/scanamo/DynamoFormatBehavior.scala
@@ -1,74 +1,93 @@
 package io.github.howardjohn.scanamo
 
-import org.scalatest.{Assertion, EitherValues, FunSuite, Matchers}
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.{ Assertion, EitherValues }
+import org.scalatest.matchers.should.Matchers
 import org.scanamo.DynamoFormat
 
-trait DynamoFormatBehavior extends Matchers with EitherValues { this: FunSuite =>
+trait DynamoFormatBehavior extends Matchers with EitherValues { this: AnyFunSuite =>
   def dynamoFormatTest[T](parse: String => Either[Any, T])(format: DynamoFormat[T]): Unit = {
     def roundTrip(input: String, expected: String): Assertion = {
-      val json = parse(input)
+      val json      = parse(input)
       val attribute = format.write(json.right.value).toAttributeValue
-      val jsonResp = format.read(attribute)
-      assert(expected === attribute.toString)
+      val jsonResp  = format.read(attribute)
+      attribute.toString should be(expected)
       assert(jsonResp === json)
     }
 
-    test("empty map")(roundTrip("{}", "AttributeValue(SS=[], NS=[], BS=[], M={}, L=[])"))
+    test("empty map")(roundTrip("{}", "AttributeValue(M={})"))
     test("integer value")(
       roundTrip(
         """{"a":1}""",
-        "AttributeValue(SS=[], NS=[], BS=[], M={a=AttributeValue(N=1, SS=[], NS=[], BS=[], M={}, L=[])}, L=[])"))
+        "AttributeValue(M={a=AttributeValue(N=1)})"
+      )
+    )
     test("string value")(
       roundTrip(
         """{"a":"b"}""",
-        "AttributeValue(SS=[], NS=[], BS=[], M={a=AttributeValue(S=b, SS=[], NS=[], BS=[], M={}, L=[])}, L=[])"))
+        "AttributeValue(M={a=AttributeValue(S=b)})"
+      )
+    )
     test("bool value")(
       roundTrip(
         """{"a":true}""",
-        "AttributeValue(SS=[], NS=[], BS=[], M={a=AttributeValue(SS=[], NS=[], BS=[], M={}, L=[], BOOL=true)}, L=[])"))
+        "AttributeValue(M={a=AttributeValue(BOOL=true)})"
+      )
+    )
     test("null value")(
       roundTrip(
         """{"a":null}""",
-        "AttributeValue(SS=[], NS=[], BS=[], M={a=AttributeValue(SS=[], NS=[], BS=[], M={}, L=[], NUL=true)}, L=[])"))
+        "AttributeValue(M={a=AttributeValue(NUL=true)})"
+      )
+    )
     test("map map")(
       roundTrip(
         """{"nested":{"a":1}}""",
-        "AttributeValue(SS=[], NS=[], BS=[], M={nested=AttributeValue(SS=[], NS=[], BS=[], M={a=AttributeValue(N=1, SS=[], NS=[], BS=[], M={}, L=[])}, L=[])}, L=[])"
-      ))
+        "AttributeValue(M={nested=AttributeValue(M={a=AttributeValue(N=1)})})"
+      )
+    )
     test("int list value")(
       roundTrip(
         """{"a":[1,2,3]}""",
-        "AttributeValue(SS=[], NS=[], BS=[], M={a=AttributeValue(SS=[], NS=[], BS=[], M={}, L=[AttributeValue(N=1, SS=[], NS=[], BS=[], M={}, L=[]), AttributeValue(N=2, SS=[], NS=[], BS=[], M={}, L=[]), AttributeValue(N=3, SS=[], NS=[], BS=[], M={}, L=[])])}, L=[])"
-      ))
+        "AttributeValue(M={a=AttributeValue(L=[AttributeValue(N=1), AttributeValue(N=2), AttributeValue(N=3)])})"
+      )
+    )
     test("string list value")(
       roundTrip(
         """{"a":["b","c","d"]}""",
-        "AttributeValue(SS=[], NS=[], BS=[], M={a=AttributeValue(SS=[], NS=[], BS=[], M={}, L=[AttributeValue(S=b, SS=[], NS=[], BS=[], M={}, L=[]), AttributeValue(S=c, SS=[], NS=[], BS=[], M={}, L=[]), AttributeValue(S=d, SS=[], NS=[], BS=[], M={}, L=[])])}, L=[])"
-      ))
+        "AttributeValue(M={a=AttributeValue(L=[AttributeValue(S=b), AttributeValue(S=c), AttributeValue(S=d)])})"
+      )
+    )
     test("mixed values")(
       roundTrip(
         """{"a":1,"b":"value"}""",
-        "AttributeValue(SS=[], NS=[], BS=[], M={a=AttributeValue(N=1, SS=[], NS=[], BS=[], M={}, L=[]), b=AttributeValue(S=value, SS=[], NS=[], BS=[], M={}, L=[])}, L=[])"
-      ))
+        "AttributeValue(M={a=AttributeValue(N=1), b=AttributeValue(S=value)})"
+      )
+    )
     test("mixed list") {
       roundTrip(
         """{"a":[1,"b",false,null]}""",
-        "AttributeValue(SS=[], NS=[], BS=[], M={a=AttributeValue(SS=[], NS=[], BS=[], M={}, L=[AttributeValue(N=1, SS=[], NS=[], BS=[], M={}, L=[]), AttributeValue(S=b, SS=[], NS=[], BS=[], M={}, L=[]), AttributeValue(SS=[], NS=[], BS=[], M={}, L=[], BOOL=false), AttributeValue(SS=[], NS=[], BS=[], M={}, L=[], NUL=true)])}, L=[])"
+        "AttributeValue(M={a=AttributeValue(L=[AttributeValue(N=1), AttributeValue(S=b), AttributeValue(BOOL=false), AttributeValue(NUL=true)])})"
       )
     }
     test("nested list") {
       roundTrip(
         """{"a":[1,[2,[3]]]}""",
-        "AttributeValue(SS=[], NS=[], BS=[], M={a=AttributeValue(SS=[], NS=[], BS=[], M={}, L=[AttributeValue(N=1, SS=[], NS=[], BS=[], M={}, L=[]), AttributeValue(SS=[], NS=[], BS=[], M={}, L=[AttributeValue(N=2, SS=[], NS=[], BS=[], M={}, L=[]), AttributeValue(SS=[], NS=[], BS=[], M={}, L=[AttributeValue(N=3, SS=[], NS=[], BS=[], M={}, L=[])])])])}, L=[])"
+        "AttributeValue(M={a=AttributeValue(L=[AttributeValue(N=1), AttributeValue(L=[AttributeValue(N=2), AttributeValue(L=[AttributeValue(N=3)])])])})"
       )
     }
-    test("just bool")(roundTrip("true", "AttributeValue(SS=[], NS=[], BS=[], M={}, L=[], BOOL=true)"))
-    test("just number")(roundTrip("1", "AttributeValue(N=1, SS=[], NS=[], BS=[], M={}, L=[])"))
-    test("just string")(roundTrip("\"string\"", "AttributeValue(S=string, SS=[], NS=[], BS=[], M={}, L=[])"))
+    test("just bool")(
+      roundTrip("true", "AttributeValue(BOOL=true)")
+    )
+    test("just number")(roundTrip("1", "AttributeValue(N=1)"))
+    test("just string")(
+      roundTrip("\"string\"", "AttributeValue(S=string)")
+    )
     test("just list")(
       roundTrip(
         "[1,2,3]",
-        "AttributeValue(SS=[], NS=[], BS=[], M={}, L=[AttributeValue(N=1, SS=[], NS=[], BS=[], M={}, L=[]), AttributeValue(N=2, SS=[], NS=[], BS=[], M={}, L=[]), AttributeValue(N=3, SS=[], NS=[], BS=[], M={}, L=[])])"
-      ))
+        "AttributeValue(L=[AttributeValue(N=1), AttributeValue(N=2), AttributeValue(N=3)])"
+      )
+    )
   }
 }
